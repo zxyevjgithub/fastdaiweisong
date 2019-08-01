bbe866b5a1a9a900be3c15e56b37188ada2e6e85
daiweisong <daiweisong-it@bestpay.com.cn>
2019-08-01 15:25:58 +0800

    Initial commit

---
+++ daiweisong-api/src/main/java/com/ddd/AccountProcessHandler.java
@@ -0,0 +1,19 @@
+package com.ddd;
+
+import org.springframework.stereotype.Service;
+
+
+/**
+ * @author devdws
+ * @version Id: AccountProcessHandler.java, v 0.1  2019/7/23 18:50 devdws Exp $$
+ */
+@Service
+public class AccountProcessHandler {
+
+
+     public void loadCache(){
+
+     }
+
+
+}
